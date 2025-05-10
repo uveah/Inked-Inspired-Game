553889f1aae881ad8968c655e73ac5d58c716354
uveah <md.uvesh.raza001@gmail.com>
2025-05-10 12:39:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true
-  },
-  build: {
-    outDir: 'dist',
-  },
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    server: {
+      host: true
+    },
+    build: {
+      outDir: 'dist',
+    },
+  };
 });
